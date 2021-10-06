f98d6ab3703f313ca4ef5202c6e3c89ea4c61393
Yemi Afolabi <yemi.afo@outlook.com>
2021-10-06 15:41:54 -0400

    Updating makeArrayConsecutive2 solution with explination and examples

--- CodeSignal Solutions/makeArrayConsecutive2.py
+++ CodeSignal Solutions/makeArrayConsecutive2.py
@@ -6,10 +6,10 @@
 # Help him figure out the minimum number of additional statues needed.
 
 def makeArrayConsecutive2(statues):
-    if (len(statues)==1):
-        return 0
+    if (len(statues)==1): # If there is only one statue....,
+        return 0 #you dont have any gaps to fill.
     else:
-        statues.sort()
+        statues.sort() # Sort the function to make it easier to find gaps in array 
         #OR
         # for i in range(len(statues)-1):
         #     if (statues[i]> statues[i+1]):
@@ -17,11 +17,21 @@ def makeArrayConsecutive2(statues):
         #         statues[i] = statues[i+1]
         #         statues[i+1]= holder
         
-        makeConsecutivecount = 0 
-        for i in range(len(statues)-1):
-            if(statues[i+1] != statues[i]+1):
-                makeConsecutivecount += ((statues[i+1] - statues[i])-1)
+        makeConsecutivecount = 0 #counter int to keep track of how many numbers are needed to make the list comsecutive
+        for i in range(len(statues)-1): #For the length of the statues list -1 because we index the list with 1 ahead of the current index
+            if(statues[i+1] != statues[i]+1): # Check if the indexes are consecutive 
+                makeConsecutivecount += ((statues[i+1] - statues[i])-1) #If not then subtract the difference of the gap -1 to get the amount needed
             else:
-                continue
+                continue # continue with the loop
                 
         return makeConsecutivecount
+
+statues = [1,2,5, 9]
+statues2 = [1]
+statues3 = [-1,2,-5, 9,6,-10]
+statues4 = [8,3,1,4,7]
+
+print(makeArrayConsecutive2(statues)) #5
+print(makeArrayConsecutive2(statues2)) #0
+print(makeArrayConsecutive2(statues3)) #14
+print(makeArrayConsecutive2(statues4)) #3
