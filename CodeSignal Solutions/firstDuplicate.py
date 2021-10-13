d04d7fdeaac3532dc65b1df9575833681c13b4c8
Yemi Afolabi <yemi.afo@outlook.com>
2021-10-13 01:07:00 -0400

    Solution that needs better time complexity, to fully pass all tests

--- CodeSignal Solutions/firstDuplicate.py
+++ CodeSignal Solutions/firstDuplicate.py
@@ -1,19 +1,16 @@
 def firstDuplicate(a):
-    # i = 0 
-    # j=
-    # lowidx =[]
-    # while (i< len(a)-1):
-    #     while ()
     if(len(a)<2):
-        return -1    
-    else:
-        lowidx = 0
-        for i in range(len(a)):
-            for j in range(i+1,len(a),1):
-                if(a[i] ==a [j] & j <= lowidx):
-                    lowidx =j
-    if(lowidx == 0):
         return -1
     else:
-        return lowidx
+        lowidx = len(a)
+        for i in range(len(a)):
+            for j in range(i+1,len(a),1):
+                if((a[i] == a[j]) & (j < lowidx)):
+                        lowidx = j
+    
+    if(lowidx == len(a)):
+        return -1
+    else:
+        return a[lowidx]
+    
     
