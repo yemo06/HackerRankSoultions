2a25e8dd1369cf003282e181d894b29c3ecbbbd1
Yemi Afolabi <yemi.afo@outlook.com>
2021-10-15 23:11:57 -0400

    Adding explination to the firstNonRepeating character solution

--- CodeSignal Solutions/firstNonReapeatingCharacters.py
+++ CodeSignal Solutions/firstNonReapeatingCharacters.py
@@ -4,14 +4,14 @@
 
 def firstNotRepeatingCharacter(s):
     
-    nonRepeat = dict.fromkeys(s,0)
-    for i in range(len(s)):
-        if(s[i] in nonRepeat):
-            nonRepeat[s[i]] +=1
+    nonRepeat = dict.fromkeys(s,0) #Creating a dictiionary from the list to hold the individual letters as key and there frequecy as values
+    for i in range(len(s)): #Loop through the length of the string
+        if(s[i] in nonRepeat): # if a letter of the string s in the list of keys in the dictionary
+            nonRepeat[s[i]] +=1 #increment the value matching string letter in the list to the one key in the dictionary
             
     
-    for letter in nonRepeat:
-        if (nonRepeat[letter] == 1):
-            return letter
+    for letter in nonRepeat: # Loop again though each letter
+        if (nonRepeat[letter] == 1): # if the letter key in the dictionary equals one 
+            return letter #Immediately return the letter as the first "Non repeating character"
     
-    return '_'
+    return '_' # If there is no "Non repeating character" return the "_"
